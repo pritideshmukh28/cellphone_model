f92b87f171250943ce7d31760a03d741b60c4323
Priti Deshmukh <priti.deshmukh2223@gmail.com>
2023-07-11 13:58:39 +0530

    updated

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-PORT_NUMBER = 5004
+PORT_NUMBER = 8080
 
 MONGODB_PORT_NUMBER = 27017
 MONGODB_DATABASE_NAME = 'project_db'
-MODEL_FILE_PATH = r'static\model\linear_regression.pkl'
+MODEL_FILE_PATH = os.path.join(r'static','model','linear_regression.pkl')
 
-JSON_FILE_PATH = r'static\json_files\proj_data.json'
+JSON_FILE_PATH = os.path.join(r'static','json_files','proj_data.json')
--- interface.py
+++ interface.py
@@ -39,4 +39,4 @@ def predict_price():
     return render_template('mobile_pred.html', prediction = pred_price)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
